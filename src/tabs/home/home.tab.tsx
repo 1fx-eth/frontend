5abaff7094d1a14fdaad301d3004083ff5032f60
Daniel de Witte <Artimunor@users.noreply.github.com>
2023-05-14 08:31:30 +0100

    fix: home

--- src/tabs/home/home.tab.tsx
+++ src/tabs/home/home.tab.tsx
@@ -14,7 +14,7 @@ export const HomeTab: React.FC = () => {
     <div className={styles["home"]}>
       <InformationSection
         icon={gauge}
-        title="DeFi trading with high leverage."
+        title="High Leverage DeFi Trading Revolutionized"
         description={onefx}
       />
       <IllustratedTextListComponent items={illustratedTextItems} />
@@ -28,17 +28,17 @@ const onefx =
 const illustratedTextItems = [
   {
     image: Leverage,
-    title: "High Leverage",
-    text: "In order to facilitate users maintaining multiple, isolated leveraged positions simultaneously, we employ abstract accounts that hold user balances. This not only streamlines user experience but also automates the deposit-borrow-swap-supply flow in lending protocols such as AAVE.",
+    title: "Up to 30x Leverage",
+    text: "1fx redefines decentralized forex trading, enabling users to maintain multiple isolated leveraged positions simultaneously. Our platform integrates abstract accounts to hold user balances, thereby simplifying user experience while automating the deposit-borrow-swap-supply cycle in lending protocols such as AAVE. Experience trading power like never before with leverage up to 30x.",
   },
   {
     image: Abstract,
     title: "Abstract Accounts",
-    text: "In order to facilitate users maintaining multiple, isolated leveraged positions simultaneously, we employ abstract accounts that hold user balances. This not only streamlines user experience but also automates the deposit-borrow-swap-supply flow in lending protocols such as AAVE.",
+    text: "At 1fx, we harness the power of abstract accounts to optimize your trading experience. These accounts are designed to hold user balances and facilitate the management of multiple isolated leveraged positions concurrently. This not only enhances the user experience but also seamlessly automates the deposit-borrow-swap-supply operations in AAVE lending protocols.",
   },
   {
     image: Fees,
     title: "Low Fees",
-    text: "In order to facilitate users maintaining multiple, isolated leveraged positions simultaneously, we employ abstract accounts that hold user balances. This not only streamlines user experience but also automates the deposit-borrow-swap-supply flow in lending protocols such as AAVE.",
+    text: "We've built 1fx to prioritize your trading success without compromising your profits. Our use of abstract accounts allows users to maintain multiple isolated leveraged positions while minimizing cost. We've streamlined the deposit-borrow-swap-supply process within AAVE lending protocols to ensure you enjoy a cost-effective trading experience. With 1fx, enjoy lower fees and higher leverage in your DeFi trading journey.",
   },
 ];
